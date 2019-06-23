7d92661abaaa0254b6da743a68b6d9ae044753ac
Josh Akeman <joshakeman@gmail.com>
2019-06-23 14:34:24 -0400

    tinkered with styling

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { ChatManager, TokenProvider } from '@pusher/chatkit-client';
 
 import Input from './Input'
+import MessageList from './MessageList';
 
 
 class ChatApp extends Component {
@@ -63,6 +64,7 @@ class ChatApp extends Component {
         return (
             <div>
                 <h2 className="header">Let's Talk</h2>
+                <MessageList messages={this.state.messages} />
                 <Input className="input-field" onSubmit={this.addMessage} />
             </div>
         )
