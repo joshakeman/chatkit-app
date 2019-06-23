b74b2cf0ac163afbdcfcaf0dd8ea347345a18d8e
Josh Akeman <joshakeman@gmail.com>
2019-06-23 11:14:07 -0400

    chatkit authorization functioninng

--- src/App.js
+++ src/App.js
@@ -3,32 +3,78 @@ import './App.css';
 
 import ChatMessage from './components/ChatMessage';
 
-    class App extends Component {
-      constructor(props) {
-          super(props);
-          this.state = {
-            currentView: 'ChatMessage'
-          }
-          this.changeView = this.changeView.bind(this);
-      }
+import { default as Chatkit } from '@pusher/chatkit-server';
 
-      changeView(view) {
+const chatkit = new Chatkit({
+  instanceLocator: "v1:us1:3a28e9bf-26c6-4a14-afa9-c74b59e740e6",
+  key: "d4bf7765-bfd1-4d61-ad71-9e1ef17c0b4b:xQq9tcw78iOpffZ13fnagzNupvOsXY0kLQ09hEdnQFA="
+})
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          user_email: '',
+          currentUsername: '',
+          currentId: '',
+          currentView: 'ChatMessage'
+      }
+      this.changeView = this.changeView.bind(this);
+      this.createUser = this.createUser.bind(this);
+  }
+
+  componentDidMount() {
+
+    //Simulating passing email as props... would be setting user_email
+    //as this.props.user_email
+    this.setState({
+        user_email: 'info@email.com'
+    })
+  }
+
+  createUser(username) {
+      chatkit.createUser({
+          id: username,
+          name: username,
+      })
+      .then((currentUser) => {
           this.setState({
-              currentView: view
+              currentUsername: username,
+              currentId: username,
+              currentView: 'chatApp'
           })
+      }).catch((err) => {
+               if(err.status === 400) {
+              this.setState({
+                  currentUsername: username,
+                  currentId: username,
+                  currentView: 'chatApp'
+              })
+          } else {
+              console.log(err.status);
+          }
+      });
+  }
+
+changeView(view) {
+    this.setState({
+        currentView: view
+    })
+}
+
+render() {
+      let view ='';
+
+      if (this.state.currentView === "ChatMessage") {
+          view = <ChatMessage  changeView={this.changeView}/>
+      } else if (this.state.currentView === "chatApp") {
+          view = <h1>The chatapp will go here</h1>
       }
-
-      render() {
-            let view ='';
-            if (this.state.currentView === "ChatMessage") {
-                view = <ChatMessage  changeView={this.changeView}/>
-            }
-            return (
-                <div className="App">
-                    {view}
-                </div>
-            );
-        }
-    }
-
-    export default App;
+      return (
+          <div className="App">
+              {view}
+          </div>
+      );
+  }
+}
+export default App;
--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -19,24 +19,59 @@ class  ChatMessage extends Component {
         }
     }
 
-    changeView = () => {
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        })
+      }
+    
+    handleCancelClose = () => {
+        this.setState({
+            open: false
+        })
+      }
+    
+    handleAuthorize = () => {
+        this.setState({
+            open: false
+        })
         this.props.changeView('chatApp')
     }
 
-    handleClickOpen = () => {
-        setOpen(true);
-      }
-    
-    handleClose = () => {
-        setOpen(false);
-      }
-
     render() {
 
-        const { classes } = this.props
+        const { classes } = this.props,
+              { open } = this.state
         return (
             <div>
-                <Button className={classes.chatButton} color="primary" variant="contained" onClick={this.handleClickOpen}>Send a message</Button>
+                <Button 
+                    className={classes.chatButton} 
+                    color="primary" 
+                    variant="contained" 
+                    onClick={this.handleClickOpen}>
+                        Send a message
+                </Button>
+                <Dialog
+                    open={open}
+                    onClose={this.handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">Authorize ChatKit to use email</DialogTitle>
+                <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    ChatKit needs to authorize you as a user via your email account before you can begin your chat session.
+                </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={this.handleAuthorize} color="primary">
+                    Authorize
+                </Button>
+                <Button onClick={this.handleCancelClose} color="default" autoFocus>
+                    Cancel
+                </Button>
+                </DialogActions>
+            </Dialog>
             </div>
         )
     }
